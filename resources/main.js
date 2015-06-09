81f874f60658837aa4605aed7e365f302638f37d
Paulo Henrique de Carvalho <paulocarvalho@dcc.ufmg.br>
2015-06-09 13:51:34 -0400

    Fixing home image on Mac

--- resources/main.js
+++ resources/main.js
@@ -78,10 +78,10 @@ BRUSHED.slider = function(){
 		thumb_links				:	0,			// Individual thumb links for each slide
 		thumbnail_navigation    :   0,			// Thumbnail navigation
 		slides 					:  	[			// Slideshow Images
-											{image : '_include/img/slider-images/image01.jpg', title : '<div class="slide-content">'+ slide_content_text +'</div>', thumb : '', url : ''},
-											{image : '_include/img/slider-images/image02.jpg', title : '<div class="slide-content">'+ slide_content_text +'</div>', thumb : '', url : ''},
-											{image : '_include/img/slider-images/image03.jpg', title : '<div class="slide-content">'+ slide_content_text +'</div>', thumb : '', url : ''},
-											{image : '_include/img/slider-images/image04.jpg', title : '<div class="slide-content">'+ slide_content_text +'</div>', thumb : '', url : ''}  
+											{image : 'img/sliderimages/image01.jpg', title : '<div class="slide-content">'+ slide_content_text +'</div>', thumb : '', url : ''},
+											{image : 'img/sliderimages/image02.jpg', title : '<div class="slide-content">'+ slide_content_text +'</div>', thumb : '', url : ''},
+											{image : 'img/sliderimages/image03.jpg', title : '<div class="slide-content">'+ slide_content_text +'</div>', thumb : '', url : ''},
+											{image : 'img/sliderimages/image04.jpg', title : '<div class="slide-content">'+ slide_content_text +'</div>', thumb : '', url : ''}  
 									],
 									
 		// Theme Options			   
