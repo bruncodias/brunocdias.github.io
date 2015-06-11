4dca448052798c3e87463131cb7381ac122cb16a
Paulo Henrique de Carvalho <paulocarvalho@dcc.ufmg.br>
2015-06-11 13:46:28 -0400

    New responsive rule for dept message
    * Slideshow disabled
    * Removing unnecessary images

--- resources/main.js
+++ resources/main.js
@@ -50,7 +50,7 @@ var slide_content_text = '日本語 Japanese at Lehman, CUNY';
 BRUSHED.slider = function(){
 	$.supersized({
 		// Functionality
-		slideshow               :   1,			// Slideshow on/off
+		slideshow               :   0,			// Slideshow on/off
 		autoplay				:	1,			// Slideshow starts playing automatically
 		start_slide             :   1,			// Start slide (0 is random)
 		stop_loop				:	0,			// Pauses slideshow on last slide
